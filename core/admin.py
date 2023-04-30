28896e0abfdb743e4c9e7b8892532bdc1af29e4c
Andrew-715 <kutuzkin98@mail.ru>
2023-04-30 17:40:35 +0800

    - Replacing ModelAdmin with UserAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
 from core.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name',)
-    list_filter = ('is_staff', 'is_active', 'is_superuser',)
-    search_fields = (
-        'username__startswith',
-        'email__startswith',
-        'first_name__startswith',
-        'last_name__startswith',
-    )
-    exclude = ('password',)
     readonly_fields = ('last_login', 'date_joined',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('email', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('Sates', {'fields': ('last_login', 'date_joined')}),
+    )
 
+
+admin.site.unregister(Group)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
