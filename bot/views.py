d6ce58bce319bbce7fd83aa393842b607abb1a98
Andrew-715 <kutuzkin98@mail.ru>
2023-07-01 12:36:53 +0800

    adding dc pull

--- bot/views.py
+++ bot/views.py
@@ -14,22 +14,22 @@ class VerificationView(GenericAPIView):
     model = TgUser
 
     def patch(self, request: Request, *args, **kwargs) -> Response:
-        serializer: TgUserSerializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        tg_user: TgUser = serializer.validated_data['tg_user']
-        tg_user.user = self.request.user
-        tg_user.save(update_fields=['user'])
-        instance_serializer: TgUserSerializer = self.get_serializer(tg_user)
-
-        TgClient().send_message(tg_user.telegram_chat_id, 'Bot token verified')
-
-        return Response(instance_serializer.data)
-
-
-        # serializer = TgUserSerializer(data=request.data)
+        # serializer: TgUserSerializer = self.get_serializer(data=request.data)
         # serializer.is_valid(raise_exception=True)
-        # tg_user: TgUser = serializer.save(user=request.user)
+        # tg_user: TgUser = serializer.validated_data['tg_user']
+        # tg_user.user = self.request.user
+        # tg_user.save(update_fields=['user'])
+        # instance_serializer: TgUserSerializer = self.get_serializer(tg_user)
         #
         # TgClient().send_message(tg_user.telegram_chat_id, 'Bot token verified')
         #
-        # return Response(serializer.data)
+        # return Response(instance_serializer.data)
+
+
+        serializer = TgUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        tg_user: TgUser = serializer.save(user=request.user)
+
+        TgClient().send_message(tg_user.telegram_chat_id, 'Bot token verified')
+
+        return Response(serializer.data)
